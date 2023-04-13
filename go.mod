36c76efd0035fc9bd96f5f9b5c14e82712394bcc
unknown <wulandariiratnaa@gmail.com>
2023-04-13 23:51:24 +0700

    day 6 add orm

--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module prakerja2
 go 1.20
 
 require (
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/labstack/echo/v4 v4.10.2 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
@@ -13,4 +16,6 @@ require (
 	golang.org/x/net v0.7.0 // indirect
 	golang.org/x/sys v0.5.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
+	gorm.io/driver/mysql v1.5.0 // indirect
+	gorm.io/gorm v1.25.0 // indirect
 )
--- main.go
+++ main.go
@@ -4,12 +4,19 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 type User struct {
-	Name string `json:"name"`
-	Age  int    `json:"age"`
+	Id      uint   `gorm:"primaryKey" json:"id"`
+	Name    string `json:"name"`    // name
+	Age     int    `json:"age"`     // age
+	Gender  string `json:"gender"`  // age
+	Address string `json:"address"` // age
 }
+
+var DB *gorm.DB
 
 type Response struct {
 	Message string      `json:"message"`
@@ -21,17 +28,45 @@ type LoginRequest struct {
 	Password string `json:"password"`
 }
 
-var data []User = []User{
-	{"alta", 10},
-	{"academy", 11},
-}
-
 func main() {
+	initDB()
 	e := echo.New()
+	e.POST("/users", AddUsers)
 	e.GET("/users", GetUsers)
 	e.GET("/users/:id", GetDetailUsers)
 	e.POST("/login", Login)
 	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func initDB() {
+	dsn := "root:123@tcp(127.0.0.1:3306)/prakerja_2?charset=utf8mb4&parseTime=True&loc=Local"
+	var err error
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("connection DB")
+	}
+	migration()
+}
+
+func migration() {
+	DB.AutoMigrate(&User{})
+}
+
+func AddUsers(c echo.Context) error {
+	var user User
+	c.Bind(&user)
+
+	result := DB.Create(&user)
+
+	if result.Error != nil {
+		return c.JSON(http.StatusInternalServerError, Response{
+			"failed", nil,
+		})
+	}
+
+	return c.JSON(http.StatusOK, Response{
+		"sucess", user,
+	})
 }
 
 func Login(c echo.Context) error {
@@ -54,10 +89,17 @@ func GetDetailUsers(c echo.Context) error {
 }
 
 func GetUsers(c echo.Context) error {
-	page := c.QueryParam("page")
-	limit := c.QueryParam("limit")
-	// logic
+	var users []User
+
+	result := DB.Find(&users)
+
+	if result.Error != nil {
+		return c.JSON(http.StatusInternalServerError, Response{
+			"failed", nil,
+		})
+	}
+
 	return c.JSON(http.StatusOK, Response{
-		"sucess", page + " & " + limit,
+		"sucess", users,
 	})
 }
